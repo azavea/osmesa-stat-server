918b655c83485c4cf7f7e5881477e3a318e8acf7
Seth Fitzsimmons <seth@mojodna.net>
2019-02-13 20:18:11 -0700

    Refresh country statistics

--- docker/osm-stat-server/refresh-views.sh
+++ docker/osm-stat-server/refresh-views.sh
@@ -11,3 +11,9 @@ if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'r
   # refresh in the background to return immediately
   psql -Aqtc "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_statistics" $DATABASE_URL &
 fi
+
+if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently country_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
+  # refresh in the background to return immediately
+  echo "$(date --iso-8601=seconds): Refreshing country statistics"
+  psql -Aqtc "REFRESH MATERIALIZED VIEW CONCURRENTLY country_statistics" $DATABASE_URL &
+fi
