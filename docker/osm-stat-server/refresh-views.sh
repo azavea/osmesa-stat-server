be7a93abf10e7dc4b9525720b97c7115441be95b
Seth Fitzsimmons <seth@mojodna.net>
2019-02-13 20:18:01 -0700

    Add logging

--- docker/osm-stat-server/refresh-views.sh
+++ docker/osm-stat-server/refresh-views.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently user_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
+  echo "$(date --iso-8601=seconds): Refreshing user statistics"
   # refresh in the background to return immediately
   psql -Aqtc "REFRESH MATERIALIZED VIEW CONCURRENTLY user_statistics" $DATABASE_URL &
 fi
 
 if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently hashtag_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
+  echo "$(date --iso-8601=seconds): Refreshing hashtag statistics"
   # refresh in the background to return immediately
   psql -Aqtc "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_statistics" $DATABASE_URL &
 fi
