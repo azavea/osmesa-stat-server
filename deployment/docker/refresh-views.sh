a48be527ae427a79899bc06346ef84fbf38f17ee
jpolchlo <jpolchlopek@azavea.com>
2019-08-21 13:13:33 -0400

    Start reorganization of deployment scripts

--- deployment/docker/refresh-views.sh
+++ deployment/docker/refresh-views.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "$(date --iso-8601=seconds): Starting view refreshment"
+
 if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently user_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
   echo "$(date --iso-8601=seconds): Refreshing user statistics"
   # refresh in the background to return immediately
@@ -7,6 +9,8 @@ if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'r
     -c "REFRESH MATERIALIZED VIEW CONCURRENTLY user_statistics" \
     -c "UPDATE refreshments SET updated_at=now() where mat_view='user_statistics'" \
     $DATABASE_URL &
+else
+  echo "$(date --iso-8601=seconds): User stats table already refreshing"
 fi
 
 if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently hashtag_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
@@ -16,6 +20,8 @@ if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'r
     -c "REFRESH MATERIALIZED VIEW CONCURRENTLY hashtag_statistics" \
     -c "UPDATE refreshments SET updated_at=now() where mat_view='hashtag_statistics'" \
     $DATABASE_URL &
+else
+  echo "$(date --iso-8601=seconds): Hashtag stats table already refreshing"
 fi
 
 if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'refresh materialized view concurrently country_statistics%' and state='active'" $DATABASE_URL 2> /dev/null)" == "0" ]; then
@@ -25,6 +31,8 @@ if [ "$(psql -Aqtc "select count(pid) from pg_stat_activity where query ilike 'r
     -c "REFRESH MATERIALIZED VIEW CONCURRENTLY country_statistics" \
     -c "UPDATE refreshments SET updated_at=now() where mat_view='country_statistics'" \
     $DATABASE_URL &
+else
+  echo "$(date --iso-8601=seconds): Country stats table already refreshing"
 fi
 
 wait
