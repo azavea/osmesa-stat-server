19cbf2c4701366c9ed47c1fc0f525c140bed7e94
jpolchlo <jpolchlopek@azavea.com>
2019-08-23 10:26:20 -0400

    Bring SQL queries in line with deployed DB schema

--- src/main/scala/osmesa/server/stats/HashtagStats.scala
+++ src/main/scala/osmesa/server/stats/HashtagStats.scala
@@ -1,18 +1,26 @@
 package osmesa.server.stats
 
+import java.time.Instant
+
 import cats.effect._
 import cats.implicits._
 import doobie._
 import doobie.implicits._
 import io.circe._
 import io.circe.generic.extras.semiauto._
+import io.circe.java8.time._
 import osmesa.server._
 import osmesa.server.model._
 
 case class HashtagStats(tag: String,
                         measurements: Json,
                         counts: Json,
-                        users: Json)
+                        changesetCount: Option[Int],
+                        editCount: Option[Int],
+                        lastEdit: Option[Instant],
+                        updatedAt: Option[Instant],
+                        userChangesets: Json,
+                        userEdits: Json)
 
 object HashtagStats extends Implicits {
   implicit val userHashtagDecoder: Decoder[HashtagStats] = deriveDecoder
@@ -24,7 +32,12 @@ object HashtagStats extends Implicits {
         tag,
         coalesce(measurements, '{}'::jsonb) measurements,
         coalesce(counts, '{}'::jsonb) counts,
-        coalesce(users, '{}'::jsonb) users
+        changeset_count,
+        edit_count,
+        last_edit,
+        updated_at,
+        coalesce(user_changesets, '{}'::jsonb) user_changesets,
+        coalesce(user_edits, '{}'::jsonb) user_edits
       FROM
         hashtag_statistics
     """
